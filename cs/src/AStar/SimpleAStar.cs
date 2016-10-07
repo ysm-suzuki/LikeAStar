3ca6269bf4026c9bec2dd3513f57e2cffda4c679
ysm-suzuki <ys443ys443@yahoo.co.jp>
2016-10-07 16:13:37 +0900

    Remove a Lambda function.

--- cs/src/AStar/SimpleAStar.cs
+++ cs/src/AStar/SimpleAStar.cs
@@ -12,63 +12,61 @@ namespace JustLikeAStar
     {
         override public Stack<Cell> Get(Cell start, Cell destination, List<Cell> cells)
         {
-            List<AStarPath> results = new List<AStarPath>();
-
             if (start.IsDisabled())
                 start.ForceReady();
             if (destination.IsDisabled())
                 destination.ForceReady();
 
-            bool isEnd = false;
-            System.Action<Cell> calcurate = null;
-            calcurate = (Cell cell) =>
+            start.Evaluate(destination);
+            Process(start, destination);
+
+            return GetMinScorePath(_results).path;
+        }
+
+        List<AStarPath> _results = new List<AStarPath>();
+        bool _isEnd = false;
+        private void Process(Cell cell, Cell destination)
+        {
+            if (_isEnd)
+                return;
+            if (cell == null)
             {
-                if (isEnd)
-                    return;
-                if (cell == null)
+
+                return;
+            }
+            if (cell == destination)
+            {
+                Stack<Cell> path = MakePath(cell);
+                _results.Add(new AStarPath
                 {
-
-                    return;
-                }
-                if (cell == destination)
-                {
-                    Stack<Cell> path = MakePath(cell);
-                    results.Add(new AStarPath
-                    {
-                        path = path,
-                        score = AStarPath.GetScore(path)
-                    });
-
-                    isEnd = true;
-                    return;
-                }
-
-                cell.Close();
-
-                List<Cell> openedCells = OpenNextCells(cell);
-                if (openedCells.Count == 0)
-                    return;
-
-                foreach (var openedCell in openedCells)
-                    openedCell.SetScore(destination);
-
-                openedCells.Sort(delegate(Cell a, Cell b)
-                {
-                    float diff = a.score - b.score;
-                    while (diff != 0
-                        && diff * diff < 1)
-                        diff *= 10;
-                    return (int)(diff);
+                    path = path,
+                    score = AStarPath.GetScore(path)
                 });
 
-                foreach (var openedCell in openedCells)
-                    calcurate(openedCell);
-            };
+                _isEnd = true;
+                return;
+            }
 
-            start.Evaluate(destination);
-            calcurate(start);
+            cell.Close();
 
-            return GetMinScorePath(results).path;
+            List<Cell> openedCells = OpenNextCells(cell);
+            if (openedCells.Count == 0)
+                return;
+
+            foreach (var openedCell in openedCells)
+                openedCell.SetScore(destination);
+
+            openedCells.Sort(delegate(Cell a, Cell b)
+            {
+                float diff = a.score - b.score;
+                while (diff != 0
+                    && diff * diff < 1)
+                    diff *= 10;
+                return (int)(diff);
+            });
+
+            foreach (var openedCell in openedCells)
+                Process(openedCell, destination);
         }
 
         // Returns Opened cells. 
